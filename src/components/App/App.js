7f239a7d584810424d30a201bd559e7fb39a1995
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-02 12:18:52 -0600

    feat: add app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,9 +4,25 @@ import Ideas from '../Ideas/Ideas'
 import Form from '../Form/Form'
 
 function App() {
-  return (
-    <h1>Welcome to IdeaBox</h1>
-  )
-}
-
-export default App;
+  let fillerIdeas = [
+    {id:1, title: 'Hug your Dog', description: 'fun activity'},
+    {id:1, title: 'Walk your Dog', description: 'exercise activity'},
+    {id:1, title: 'Play with your Dog', description: 'stimulating activity'}
+  ]
+  const [ideas, setIdeas] = useState([])
+  
+  function addIdea(newIdea) {
+  setIdeas([...ideas, newIdea])
+  }
+  
+    return (
+      <main className="App">
+      <h1>IdeaBox</h1>
+      <Form addIdea={addIdea}/>
+      {!ideas.length && <h2>No ideas yet -- add some!</h2>}
+      <Ideas ideas={ideas}/>
+      </main>
+    )
+  }
+  
+  export default App;
