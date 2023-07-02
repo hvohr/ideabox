72ffc286ea7e3b1c9e2c45ac677c4e20cb25006b
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-02 14:05:47 -0600

    feat: add delete features

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -2,17 +2,19 @@ import './Ideas.css'
 import Card from '../Card/Card'
 
 
-function Ideas({ideas}) {
-  const ideaCards = ideas.map(idea => {
-  return (
-      <Card 
+const Ideas = (props) => {
+  const ideaCards = props.ideas.map(idea => {
+    return (
+      <Card
         title={idea.title}
         description={idea.description}
         id={idea.id}
         key={idea.id}
+        deleteIdea= {props.deleteIdea}
       />
-  )
-})
+    )
+  })
+
 return (
   <div className='ideas-container'>
     {ideaCards}
