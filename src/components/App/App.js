b805b464f0e0f700abfc457f3ee180724dc69038
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-02 12:15:42 -0600

    feat: add card function component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import Ideas from '../Ideas/Ideas'
+import Form from '../Form/Form'
 
 function App() {
   return (
-    
+    <h1>Welcome to IdeaBox</h1>
   )
 }
 
---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+import './Ideas.css'
+import Card from '../Card/Card'
+
+
+function Ideas({ideas}) {
+  const ideaCards = ideas.map(idea => {
+  return (
+      <Card 
+        title={idea.title}
+        description={idea.description}
+        id={idea.id}
+        key={idea.id}
+      />
+  )
+})
+return (
+  <div className='ideas-container'>
+    {ideaCards}
+  </div>
+)
+}
